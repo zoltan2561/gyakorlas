b45d1ab17396a2c868ae0fe2935b26c636602dcb
zoltan2561 <pappzoltan6969@gmailcom>
2022-11-15 14:39:18 +0100

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//       int number=2+3*4;
+//        System.out.println(number);
+//boolean b= number+10<100;
+//        System.out.println(b);
+//int i=100;
+//boolean b1=true;
+//boolean ered =b1 && i++<1000;
+//        System.out.println(ered);
+//        System.out.println(i);
+//        int i=100;
+//        int j=199;
+//        i=j;
+//        System.out.println(i<200 ? "kisebb": "nagyobb" );
+    int[] szam = new int[10];
+
+        szam[0] = 1;
+        szam[1] = 2;
+        szam[2] = 3;
+        szam[3] = 4;
+        szam[4] = 5;
+        szam[5] = 6;
+        szam[6] = 7;
+        szam[7] = 8;
+        szam[8] = 9;
+        szam[8] = 10;
+        System.out.println(szam[5]);
+
+
+
+
+
+    }
+
+
+}
