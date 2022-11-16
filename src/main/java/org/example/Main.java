2b6b4c8587883b6039f0f75c3313a23d65dc9cd8
zoltan2561 <pappzoltan6969@gmailcom>
2022-11-16 18:48:38 +0100

    12365

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,25 +15,100 @@ public class Main {
 //        int j=199;
 //        i=j;
 //        System.out.println(i<200 ? "kisebb": "nagyobb" );
-    int[] szam = new int[10];
+//    int[] szam = new int[10];
+//
+//        szam[0] = 1;
+//        szam[1] = 2;
+//        szam[2] = 3;
+//        szam[3] = 4;
+//        szam[4] = 5;
+//        szam[5] = 6;
+//        szam[6] = 7;
+//        szam[7] = 8;
+//        szam[8] = 9;
+//        szam[8] = 10;
+//        System.out.println(szam[5]);
+//
 
-        szam[0] = 1;
-        szam[1] = 2;
-        szam[2] = 3;
-        szam[3] = 4;
-        szam[4] = 5;
-        szam[5] = 6;
-        szam[6] = 7;
-        szam[7] = 8;
-        szam[8] = 9;
-        szam[8] = 10;
-        System.out.println(szam[5]);
+      //  int[][] szam = new int[10][10];
+
+//////////////////////////////////////////////////////////////////////////////CONTROLFLOW//////////////////////
+//        int honapnap = 4;
+//        System.out.println("üdv!");
+//        if(honapnap== 2){
+//            System.out.println("hétfő");
+//
+//        }
+//        if(honapnap==3)
+//            System.out.println("kedd");
+//        System.out.println("viszlat");
+//
+//if(honapnap== 4){
+//    System.out.println("4");
+//
+//}else{
+//    System.out.println("nem negy");
+//}
+//    } ///////////////////////////////////////////////////////////////////// 3 közül kiértékelés
+//int szam =0;
+//if(szam>0){
+//    System.out.println("a szam pozitiv");
+//}else if(szam<0){
+//    System.out.println("a szam negativ");
+//}else {
+//    System.out.println("a szam nulla");
+//}
+////////////////////////////////////////////////////////////////////////Switchcase
+//int nap=3;
+//switch ( nap) {
+//    case 1 :
+//        System.out.println("hétfő");break;
+//    case 2 :
+//        System.out.println("k");break;
+//    case 3 :
+//        System.out.println("sző");break;
+//    case 4 :
+//        System.out.println("cs");break;
+//    case 5 :
+//        System.out.println("p");break;
+//    case 6 :
+//        System.out.println("szo");break;
+//    case 7 :
+//        System.out.println("vas");break;
+//}
+
+
+/////////////////////////////////////////////////////////////////////stringes switch
+//int nap =8;
+//String napneve = switch (nap){
+//    case 1 -> "hétfő";
+//    case 2 -> "kedd";
+//    case 3 -> "szerda";
+//    case 4 -> "csüü";
+//    case 5 -> "péntek";
+//    default -> "ismeret";
+//};
+////        System.out.println(napneve);
+//        System.out.println("üdv");
+        /////////////////////////////////////////////////////////////forciklus
+//for(int i=0;i<10;i++){
+//    System.out.print("a szam:");
+//    System.out.print(i);
+//    System.out.println();
+//}
+//////////////////////////////////////////////////////////////////forciklus stringes tömbel
+//  String[] names = { "feri","zoli","vivi","zoe"};
+//  for (String nev : names){
+//      System.out.print("neve:");
+//      System.out.print(nev);
+//      System.out.println();
+//  }
+
 
 
 
 
 
     }
-
 
 }
